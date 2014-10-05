a00ab1917cbd985e8ff61f6c2391e11c3262095d
Supun Budhajeewa <budhajeewa@gmail.com>
2014-10-05 19:21:18 +0530

    Create basic pages.

---
+++ app/app.js
@@ -0,0 +1,24 @@
+angular
+.module('app', [
+	'ui.router'
+])
+.config([
+	'$urlRouterProvider',
+	'$stateProvider'
+	, function ($urlRouterProvider, $stateProvider) {
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'app/views/home.html'
+		})
+		.state('about', {
+			url: '/about',
+			templateUrl: 'app/views/about.html'
+		})
+		.state('contact', {
+			url: '/contact',
+			templateUrl: 'app/views/contact.html'
+		})
+	}])
