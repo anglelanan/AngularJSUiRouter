c303a205242f184abc2f5b639bc8fecb216d1fdf
Supun Budhajeewa <budhajeewa@gmail.com>
2014-10-07 07:23:24 +0530

    Use provided app reference in AboutController.

--- app/controllers/AboutController.js
+++ app/controllers/AboutController.js
@@ -1,6 +1,5 @@
 define(['app'], function (app) {
-	angular
-	.module('app')
+	app
 	.controller('AboutController', ['$scope', function ($scope) {
 			$scope.title = "About";
 			$scope.things = ['Thing One', 'Thing Two', 'Thing Three'];
